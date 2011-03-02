e25d3cfbe605d8ca0a0774a5d23c7906282a13ec
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-03-02 16:14:15 -0300

    Disabled Tempfile monkey-patch for Ruby >= 1.9 and another small fix to convert Filepath to String in file_system_backend.rb

--- init.rb
+++ init.rb
@@ -1,10 +1,12 @@
 require 'tempfile'
 
-Tempfile.class_eval do
-  # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
-  def make_tmpname(basename, n)
-    ext = nil
-    sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n, ext)
+if RUBY_VERSION < '1.9'
+  Tempfile.class_eval do
+    # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
+    def make_tmpname(basename, n)
+      ext = nil
+      sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n.to_i, ext)
+    end
   end
 end
 
